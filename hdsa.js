09d0d6f45c3a6e1885652555536d9d0dff69cc57
Darcy McLean <32240695+d-mclean@users.noreply.github.com>
2018-08-15 13:49:24 -0400

    Re-uploaded the main file.
    Minor formatting and commenting changes.

--- hdsa.js
+++ hdsa.js
@@ -1,9 +1,9 @@
-/*  20180814 Darcy McLean                                                       */
-/*  HubDoc Skills Assessment                                                     */
+/*  20180815 Darcy McLean                                                       */
+/*  HubDoc Skills Assessment Assignment                                         */
 /*  This assignment:                                                            */
 /*  1. navigates to http://datatables.net via google search ("datatables").     */
 /*  2. fetches/extracts data from the table into an array.                      */
-/*  3. exports results as array via CSV.                                        */
+/*  3. exports results as array via CSV (results.csv).                          */
 
 // Require Puppeteer node library to run a headless copy of Chrome.
 const puppeteer = require('puppeteer');
@@ -11,6 +11,7 @@ const puppeteer = require('puppeteer');
 // Require FileSystem to export the results as a CSV file. 
 const fs = require('fs');
 
+// Notify user when starting.
 console.log('Starting Web Scraper...')
 
 let webscraper = async () => {
@@ -19,25 +20,30 @@ let webscraper = async () => {
     const page = await browser.newPage();
 
     // Navigate to google for searching in an appropriate sized window.
-    //await page.goto('http://www.google.com/');
     await page.setViewport({width: 1000, height: 500})
     await page.goto('https://google.com', { waitUntil: 'networkidle0' });
 
-    // Type datatables into the google search inputbox.
+    // Type datatables into the google search inputbox (including a space to prevent intellisense triggering).
     await page.type('#lst-ib', 'datatables ');
 
     // Click search.
     await page.click('input[type="submit"]');
 
+    // Wait for results from Google.
     await page.waitForSelector('h3 a');
     
+    // Click on the first result from the Google search.
     const datapage = await page.$$('h3 a');
     await datapage[0].click();
+
+    // Wait for the datatables page to load with the example table.
     await page.waitForSelector('#example_wrapper');
 
+    // Loop through the table via map collecting each row from the HTML table.
     const result = await page.evaluate(() => {
         let tableData = Array.prototype.map.call(document.querySelectorAll('table tr'), function(tr){
             return Array.prototype.map.call(tr.querySelectorAll('td'), function(td){
+                // Only return the text from the table cells.
                 return td.innerHTML;
             });
         });
@@ -46,33 +52,35 @@ let webscraper = async () => {
         tableData.shift();
         tableData.pop();
 
-        return tableData; // Return our data array
+        // Return our data array.
+        return tableData;
     });
 
     // Close the browser once done.
     browser.close();
 
-
-    //console.log(result[2]);
+    // Return the table results so it can be output as a CSV file later.
     return result;
-    // Return "test".
-    //return "test";
-    //return strTest; // Return our data array
 };
 
 webscraper().then((value) => {
-    //console.log(value); // Success!
-    // fs.writeFile('output.csv', value, 'utf8', function (err) {
-    //     console.log('Results Exported!')
-    //   })
-    let file = fs.createWriteStream('output.csv');
-    file.on('error', function(err) { /* error handling */
+    // Create file for outputting results.
+    let file = fs.createWriteStream('results.csv');
+    file.on('error', function(err) { 
+        // Basic error handling (i.e. for problems creating the file (locked or r/o)).
         console.log('Error exporting file!\nWeb Scraper Failed.') 
     });
-    value.forEach(function(v) { file.write(v.join(', ') + '\n'); });
+
+    // Add each 'row' of the table with a newline at the end.
+    value.forEach(function(v) {
+        file.write(v.join(', ') + '\n'); 
+    });
+
+    // Notify user when complete.
     file.on('finish', function () {
         console.log('Results exported successfully!\nWeb Scraper Finished.');
       });
-    file.end();
 
+    // Close the file.
+    file.end();
 });
