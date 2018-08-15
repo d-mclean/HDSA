2e4cf1824c824ea080b93236c8159d9a456a0337
Darcy McLean <32240695+d-mclean@users.noreply.github.com>
2018-08-15 14:43:24 -0400

    Changed code when clicking the search button.
    Modified the code that does a google search to press enter instead of clicking search to avoid the autocomplete feature.

--- hdsa.js
+++ hdsa.js
@@ -21,14 +21,15 @@ let webscraper = async () => {
     const page = await browser.newPage();
 
     // Navigate to google for searching in an appropriate sized window.
-    await page.setViewport({width: 1000, height: 500})
+    await page.setViewport({width: 1000, height: 500});
     await page.goto('https://google.com', { waitUntil: 'networkidle0' });
 
-    // Type datatables into the google search inputbox (including a space to prevent intellisense triggering).
+    // Type datatables into the google search inputbox (with a space to assist avoiding autocomplete).
     await page.type('#lst-ib', 'datatables ');
 
-    // Click search.
-    await page.click('input[type="submit"]');
+    // Clicking search (i.e. [await page.click('input[type="submit"]');]) can incorrectly 
+    // use an autocomplete suggestion so press enter via the keyboard instead.
+    await page.keyboard.press('Enter');
 
     // Wait for results from Google.
     await page.waitForSelector('h3 a');
@@ -69,7 +70,7 @@ webscraper().then((value) => {
     let file = fs.createWriteStream('results.csv');
     file.on('error', function(err) { 
         // Basic error handling (i.e. for problems creating the file (locked or r/o)).
-        console.log('Error exporting file!\nWeb Scraper Failed.') 
+        console.log('Error exporting file!\nWeb Scraper Failed.');
     });
 
     // Add each 'row' of the table with a newline at the end.
