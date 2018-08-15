cacf785f47350c13ed13dd8868b6beb4faef780d
Darcy McLean <32240695+d-mclean@users.noreply.github.com>
2018-08-15 13:39:12 -0400

    Test file uploaded (not part of actual project)

---
+++ scrape.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+let scrape = async () => {
+  // Actual Scraping goes Here...
+  
+  // Return a value
+  return 'test';
+};
+
+scrape().then((value) => {
+    console.log(value); // Success!
+});
